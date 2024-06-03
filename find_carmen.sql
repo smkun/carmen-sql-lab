5bc5baca951e19280408aff0680a3ca6a05e83e4
smkun <skunian@yahoo.com>
2024-06-03 16:22:05 -0400

    another type

--- find_carmen.sql
+++ find_carmen.sql
@@ -55,7 +55,7 @@ WHERE population = 91084;
 -- In a city of ninety-one thousand and now, eighty five.
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
-"Commit: CARMEN SANDIEGO - I found Carmen Sandiego - Sanata Monica, Brazil"
+"Commit: CARMEN SANDIEGO - I found Carmen Sandiego - Sanata Monica"
 
 UPDATE city
 SET name = 'Brasília'
